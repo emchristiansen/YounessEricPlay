1e06517e6e819b2d43122e53193c00125f9fed55
Eric Christiansen <ericmartinchristiansen@gmail.com>
2014-12-27 14:29:06 -0800

    stuff

--- lib.py
+++ lib.py
@@ -1,1 +1,10 @@
 # This file contains helper functions, to keep the main file clean.
+
+def time_in_hours_between_locations(start, stop):
+  """
+  This returns the time it takes to drive from start to stop.
+  It finds this time by calling the Google Distance Matrix API.
+  """
+
+  # TODO(youness)
+  return 0.0
--- lib_test.py
+++ lib_test.py
@@ -6,7 +6,8 @@ class TestGoogleAPI(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_api_call_returns_sane_result(self):
+    def test_time_in_hours_between_locations(self):
+        # TODO(youness)
         self.assertEqual(False, True)
 
 if __name__ == '__main__':
