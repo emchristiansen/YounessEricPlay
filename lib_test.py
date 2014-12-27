2612be0b0ed49f827e30eec20ddd47b2b2daaa8a
Eric Christiansen <ericmartinchristiansen@gmail.com>
2014-12-27 14:34:47 -0800

    stuff

--- lib_test.py
+++ lib_test.py
@@ -2,13 +2,23 @@
 
 import unittest
 
+from lib import *
+
 class TestGoogleAPI(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_time_in_hours_between_locations(self):
         # TODO(youness)
-        self.assertEqual(False, True)
+        start = "Carlsbad, California"
+        stop = "San Francisco, California"
+
+        hours = time_in_hours_between_locations(start, stop)
+
+        print(hours)
+
+        self.assertGreater(hours, 4.0)
+        self.assertLess(hours, 16.0)
 
 if __name__ == '__main__':
     unittest.main()
